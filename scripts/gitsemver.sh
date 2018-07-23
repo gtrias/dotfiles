9b16b09e721ac9bfd0ed7d89f36bbe426524919e
Genar Trias Ortiz <genar@acs.li>
2018-07-23 11:41:09 +0200

    Fixing git-semver configuration

--- scripts/gitsemver.sh
+++ scripts/gitsemver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(git clone git@github.com:markchalloner/git-semver.git && cd git-semver && git checkout $( git tag | grep '^[0-9]\+\.[0-9]\+\.[0-9]\+$' | sort -t. -k 1,1n -k 2,2n -k 3,3n | tail -n 1) && sudo ./install.sh)
+(git clone git@github.com:markchalloner/git-semver.git && cd git-semver && sudo ./install.sh)
