3149ab29f0144e18a2d52ebeb6ef838bef37b680
Genar Trias Ortiz <gtrias@gmail.com>
2024-02-13 13:59:43 +0100

    fix esc in terminal mode

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -11,3 +11,7 @@ vim.keymap.set("v", ">", ">gv", { silent = true })
 -- Move selected line / block of text in visual mode
 vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", { silent = true })
 vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", { silent = true })
+
+-- Fix terminal mode return to normal mode
+-- :tnoremap <Esc> <C-\><C-n>
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
